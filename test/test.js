cb92467ecceeef7c0eecc820423dd63ec34f6406
akash <akash@startupwind.com>
2024-10-26 12:21:20 +0530

    add function

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
 class StringCalculate {
-
+    add(value) {
+        if (!value) return 0;
+        return value
+    }
 }
+
+const calculator = new StringCalculator();
+console.log(calculator.add());
