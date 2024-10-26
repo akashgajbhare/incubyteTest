c55e1ad54cf48397895f0b7d44bca3ef909c3ec2
akash <akash@startupwind.com>
2024-10-26 12:38:50 +0530

    second point is cover i.e  handle new lines between numbers (instead of commas). ("1\n2,3"

--- test/test.js
+++ test/test.js
@@ -1,12 +1,23 @@
 class StringCalculate {
     add(value) {
         if (!value) return 0;
-        if(value){
-            return value.length;
-        }
-       
+
+        const delimiter = /,|\n/; 
+
+        const numberList = value.split(delimiter);
+        
+        let total = 0;
+        console.log(numberList);
+        numberList.forEach(number => {
+            if (number) {  
+                total += parseInt(number, 10); 
+            }
+        });
+
+        return total;
     }
 }
 
 const calculator = new StringCalculate();
-console.log(calculator.add("12"));
+console.log(calculator.add("1,2"));
+console.log(calculator.add("1\n2,3"));
