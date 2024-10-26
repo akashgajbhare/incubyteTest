d937a87aa19a98588fd44a9c5d225de5ed7092d0
akash <akash@startupwind.com>
2024-10-26 12:55:22 +0530

    3. change the delimiter, the beginning of the string will contain a separate line that looks like this: "//[delimiter]\n[numbers…]" changes

--- test/test.js
+++ test/test.js
@@ -2,15 +2,20 @@ class StringCalculate {
     add(value) {
         if (!value) return 0;
 
-        const delimiter = /,|\n/; 
+        let delimiter = /,|\n/;
+
+        if (value.startsWith("//")) {
+            const parts = value.split("\n", 2);
+            delimiter = new RegExp(parts[0].slice(2)); 
+            value = parts[1];
+        }
 
         const numberList = value.split(delimiter);
-        
         let total = 0;
-        console.log(numberList);
+
         numberList.forEach(number => {
             if (number) {  
-                total += parseInt(number, 10); 
+                total += parseInt(number, 10);
             }
         });
 
@@ -21,3 +26,4 @@ class StringCalculate {
 const calculator = new StringCalculate();
 console.log(calculator.add("1,2"));
 console.log(calculator.add("1\n2,3"));
+console.log(calculator.add("//;\n1;2"));
